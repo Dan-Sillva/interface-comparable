33b042ff3797c4ab58ea6e848323e0331141a28b
Dan-Sillva <danilo.official22@hotmail.com>
2021-09-07 21:39:18 -0300

    version-2

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,26 +3,30 @@ package application;
 import java.io.*;
 import java.util.*;
 
+import entities.Employee;
+
 public class Program {
 
 	public static void main (String[] Args){
 		
-		List<String> list = new ArrayList<>(); 
+		List<Employee> list = new ArrayList<>(); 
 		String path = "C:\\Users\\Microsoft\\Downloads\\in.txt";
 		
 		try(BufferedReader br = new BufferedReader( new FileReader(path))){
 			
-			String name = br.readLine();
-			while (name != null) {
+			String employee = br.readLine();
+			while (employee != null) {
 				
-				list.add(name);
-				name = br.readLine();
+				String[] info = employee.split(",");
+				
+				list.add(new Employee(info[0],Double.parseDouble(info[1])));
+				employee = br.readLine();
 				
 			}
 			
 			Collections.sort(list);
-			for (String s : list) {
-				System.out.println("Name : " + s); 
+			for (Employee s : list) {
+				System.out.println(s.getName()+" : "+s.getSalary()); 
 			}
 			
 		} catch (IOException e) {
